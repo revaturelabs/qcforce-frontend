22818fad57fe3eeedde5e88f4b906e18e5b167ff
Gautam <gramasu000@citymail.cuny.edu>
2020-06-26 13:19:14 -0400

    Provided brief documentation for MenuItem interface and HeaderState state

--- src/app/interfaces/menu-item.interface.ts
+++ src/app/interfaces/menu-item.interface.ts
@@ -1,3 +1,13 @@
+
+/**
+ * MenuItem - Objects of this type will represent information
+ * links in the header menu or submenu
+ *
+ * This will be used in HeaderState, which will ultimately be used
+ *  by the NgRx state store to keep track of the state of the header
+ *  component
+ */
+
 export interface MenuItem {
   name: string;
   //action: HeaderAction;
--- src/app/states/header.state.ts
+++ src/app/states/header.state.ts
@@ -1,5 +1,13 @@
 import { MenuItem } from '../interfaces/menu-item.interface';
 
+/**
+ * HeaderState - Objects of this type will hold information about
+ *  all the links in the header menu and submenu
+ * 
+ * This is an interface that is used by NgRx state store to keep track
+ *  of the state of the header component.
+ *
+ */
 export interface HeaderState {
   menuList: MenuItem[];
   subMenuList: MenuItem[];
