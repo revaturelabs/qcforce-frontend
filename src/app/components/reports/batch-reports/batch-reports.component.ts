f8cb0c7375f90895b7de9de8af0647eda27c0d5e
acap613 <acap613@gamil.com>
2020-06-29 17:12:21 -0400

    pushing up incomplete work

--- src/app/components/reports/batch-reports/batch-reports.component.ts
+++ src/app/components/reports/batch-reports/batch-reports.component.ts
@@ -9,6 +9,9 @@ import { ChartDataSets, ChartOptions } from 'chart.js';
 })
 export class BatchReportsComponent implements OnInit {
 
+  weeklyInfo = 'Reports By Week';
+  weekDisplayed = false;
+
   public lineChartData: ChartDataSets[] = [
     { data: [3.25, 2, 5, 4, 3, 5, 1], label: 'Batch 1'},
     { data: [2.47, 3.81, 4.81, 2.11, 4.13, 3.51, 1.89], label: 'Batch 2'},
@@ -18,7 +21,20 @@ export class BatchReportsComponent implements OnInit {
   public lineChartOptions = {
     responsive: true,
   };
-  
+  public lineChartColor: Color[] =[
+    {
+      borderColor: 'black',
+      backgroundColor: 'rgba(255,110,0,1)',
+    },
+    {
+      borderColor: 'black',
+      backgroundColor: 'rgba(200,255,0,1)',
+    },
+    {
+      borderColor: 'black',
+      backgroundColor: 'rgba(100, 200, 155,1)',
+    },
+  ];
   public lineChartLegend = true;
   public lineChartType = 'bar';
   public lineChartPlugins = [];
@@ -27,7 +43,17 @@ export class BatchReportsComponent implements OnInit {
   public doughnutChartData: MultiDataSet  = [
       [6, 1, 2],
   ];
- 
+
+  public doughnutChartColor: Color[] = [
+    {
+      borderColor: 'gray',
+      backgroundColor: [
+        'rgba(255, 70, 70, 1)',
+        'rgba(200, 255, 0, 1)',
+        'rgba(100, 200, 100, 1)',
+      ]
+    }
+  ];
 
   public doughnutChartLegend = true;
   public doughnutChartType = 'doughnut';
@@ -38,4 +64,7 @@ export class BatchReportsComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  toggleWeek(){
+    this.weekDisplayed = true;
+  }
 }
