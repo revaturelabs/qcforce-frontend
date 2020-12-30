38895a41fa97b09f98ee6ee67b1296cdd45cd5d1
arac29 <almarosa_ac@hotmail.com>
2020-12-30 15:13:43 -0500

    short-answer fix

--- src/app/components/survey-form/question/short-answer/short-answer.component.ts
+++ src/app/components/survey-form/question/short-answer/short-answer.component.ts
@@ -3,8 +3,8 @@ import { FormControl, FormGroup, Validator, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-answers',
-  templateUrl: './answers.component.html',
-  styleUrls: ['./answers.component.css']
+  templateUrl: './short-answer.component.html',
+  styleUrls: ['./short-answer.component.css']
 })
 export class ShortAnswerComponent implements OnInit {
 
