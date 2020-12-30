3bc3c0547508f7284ce1ea922be1e9c73e8fd15f
arac29 <almarosa_ac@hotmail.com>
2020-12-29 20:10:03 -0500

    models and components for surveys

--- src/app/models/question.model.ts
+++ src/app/models/question.model.ts
@@ -1,8 +1,10 @@
+/**
+ * Abstract representation of associate in caliber data
+ */
 export interface Question {
     id?: number;
     createdOn?:Date;
     type?:String;
     version?:number;
     question?: String[];
-
 }
