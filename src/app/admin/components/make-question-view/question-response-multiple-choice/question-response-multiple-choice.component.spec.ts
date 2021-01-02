ad5809d92b2ead42cfd61c953710a2b81ece4e25
aholliday <acacia.holliday@snhu.edu>
2021-01-02 13:53:36 -0500

    Add tests to multiple choice component

--- src/app/admin/components/make-question-view/question-response-multiple-choice/question-response-multiple-choice.component.spec.ts
+++ src/app/admin/components/make-question-view/question-response-multiple-choice/question-response-multiple-choice.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
 
 import { QuestionResponseMultipleChoiceComponent } from "./question-response-multiple-choice.component";
 
@@ -28,4 +29,31 @@ describe("QuestionResponseMultipleChoiceComponent", () => {
         fixture.nativeElement.querySelector("#multipleChoice").toBeTruthy()
       );
     };
+
+  it("should have a submit button"),
+    () => {
+      expect(fixture.nativeElement.querySelector("#submit").toBeTruthy());
+    };
+
+  it("should have textarea for question"),
+    () => {
+      expect(
+        fixture.nativeElement.querySelector("#questionInput").toBeTruthy()
+      );
+    };
+
+  it("should have valid input when submit button is clicked"),
+    () => {
+      fixture.detectChanges();
+      const button = fixture.debugElement.query(By.css("#submitButton"));
+      expect(button.nativeElement.disabled).toBeTruthy();
+    };
+
+  it("should enable button when textArea is not empty", () => {
+    fixture.nativeElement.query("#inputQuestion").innerText =
+      "I love this test";
+    fixture.detectChanges();
+    const button = fixture.debugElement.query(By.css("button"));
+    expect(button.nativeElement.disabled).toBeFalsy();
+  });
 });
