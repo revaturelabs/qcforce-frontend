e980bd7176bda06a403495364a409aa40954ecf3
Michael Zide <michaelczide@gmail.com>
2021-01-02 14:02:13 -0600

    refactored some of the multi-choice code

--- src/app/admin/components/make-question-view/question-response-multiple-choice/question-response-multiple-choice.component.ts
+++ src/app/admin/components/make-question-view/question-response-multiple-choice/question-response-multiple-choice.component.ts
@@ -1,20 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-question-response-multiple-choice',
-  templateUrl: './question-response-multiple-choice.component.html',
-  styleUrls: ['./question-response-multiple-choice.component.css']
+  selector: "app-question-response-multiple-choice",
+  templateUrl: "./question-response-multiple-choice.component.html",
+  styleUrls: ["./question-response-multiple-choice.component.css"],
 })
 export class QuestionResponseMultipleChoiceComponent implements OnInit {
+  
   addChoiceClicked = false;
-  constructor() { }
+  questionInput: string = '';
+  completeQuestion = [];
+  
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   showChoiceMaker() {
-    this.addChoiceClicked = !this.addChoiceClicked
+    this.addChoiceClicked = !this.addChoiceClicked;
+    //Add questionInput to completeQuestion
+
   }
-
-
+  onInput(event: any, questionInput: string){
+    if (event.target.keyboardEvent){
+      questionInput = 'hello';
+    }
+  }
 }
